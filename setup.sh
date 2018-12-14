75bc6fc018e7971b6ffb5f3f84fca051ad211607
mexsser <mexsser@gmail.com>
2018-12-14 22:40:56 +0100

    Alpha Version

--- setup.sh
+++ setup.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 set -e
-
+current_dir=$(getCurrentDir)
+source $current_dir/functions.sh
 # ======= Your Data =========
 # host info
-HOST_NAME="DOBOX"
-WELCOME_MESSAGE='./welcome_message'
+HOST_NAME="DOVPS"
 # user info
-USERNAME="inje"
+USERNAME="test"
 PASSWARD='k?j0r_g^39Ge2'
-AS_SUDO_USER=true
+AS_SUDO_USER=false
 # ssh info
-PUBLIC_SSH_KEY='ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAhi54eXlkwelbgsdOzKdaNkZZIvr1sFa0THwVVjVhx4d0UIi7E5D1R9ynIxwkkVHpdmOil7OEEB8hkR/LZcxD1s2XSCHJJIBL8DEllJYtR73ng3PU1tMdl+xj+wLa9GY+pg3ZuoU//uIizF5BW02XSFtsa60qbJ5dsJ3AcgCc5xrL0C5JZIqCAZauI0R/y+2NEEM43H8ifClohMDOJXYM0St1DizQgBA5VvtRLRyLXIpZjpMnvtks9iRrTRL5fW4jwVEroYkCNONYNwhyLIfRT6X+TBCQWxC1WkDMLyblb6+RnpXLQdsqGEDJUQvoHmybacw/2ScJyiQ04IxFYCdoHQ== rsa-key-20170824'
 SSH_PORT="2020"
 # ============================
+
+WELCOME_MESSAGE="${current_dir}/welcome_message"
+PUBLIC_SSH_KEY="${current_dir}/publickey"
 
 # =========== action =========
 # update system
 apt-get update
 apt-get upgrade -y
-source ./functions.sh
+
 Menu
